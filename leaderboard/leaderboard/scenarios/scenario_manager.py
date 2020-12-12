7ac34c0b6beef1ab677721836197834bcb49169e
happyOBO <obo314@icloud.com>
2020-12-12 10:57:28 +0900

    simplify

--- leaderboard/leaderboard/scenarios/scenario_manager.py
+++ leaderboard/leaderboard/scenarios/scenario_manager.py
@@ -118,6 +118,7 @@ class ScenarioManager(object):
 
         self._agent.setup_sensors(self.ego_vehicles[0], self._debug_mode)
 
+
     def run_scenario(self,args):
         """
         Trigger the start of the scenario and wait for it to finish/fail
@@ -127,15 +128,18 @@ class ScenarioManager(object):
 
         self._watchdog.start()
         self._running = True
-
         while self._running:
             timestamp = None
             world = CarlaDataProvider.get_world()
+            
 
             if world:
-                ego_vehicle = self.ego_vehicles[0] 
+                ego_vehicle = self.ego_vehicles[0]
+                control = ego_vehicle.get_control()
+                ego_location = ego_vehicle.get_location()
                 for vehicle in world.get_actors().filter('vehicle.*'):
                     # draw Box 
+                    # ego_location = ego_vehicle.get_location()
                     if(ego_vehicle.id != vehicle.id and vehicle.is_alive):
                         if((ego_vehicle.get_location().x - vehicle.get_location().x)**2 + (ego_vehicle.get_location().y - vehicle.get_location().y)**2  < 2500):
                             transform = vehicle.get_transform()
@@ -147,66 +151,36 @@ class ScenarioManager(object):
 
                             # check front 10m
                             yaw = ego_vehicle.get_transform().rotation.yaw
+                            # print(yaw,ang)
                             interval_front_point = carla.Location(ego_vehicle.get_location().x + 10.0 * math.cos(math.radians(yaw)) , ego_vehicle.get_location().y + 10.0 * math.sin(math.radians(yaw)) ,0.0)
-                            interval_left_point = carla.Location(ego_vehicle.get_location().x + 3.0 * math.cos(math.radians(yaw - 90.0)) , ego_vehicle.get_location().y + 3.0 * math.sin(math.radians(yaw- 90.0)) ,0.0)
-                            interval_right_point = carla.Location(ego_vehicle.get_location().x + 3.0 * math.cos(math.radians(yaw + 90.0)) , ego_vehicle.get_location().y + 3.0 * math.sin(math.radians(yaw + 90.0)) ,0.0)
+                            interval_left_point = carla.Location(ego_vehicle.get_location().x + 3.0 * math.cos(math.radians(yaw + 90)) , ego_vehicle.get_location().y + 3.0 * math.sin(math.radians(yaw + 90)) ,0.0)
+                            interval_right_point = carla.Location(ego_vehicle.get_location().x + 3.0 * math.cos(math.radians(yaw - 90.0)) , ego_vehicle.get_location().y + 3.0 * math.sin(math.radians(yaw - 90.0)) ,0.0)
                             # 기울기
+                            order = ["front" , "left", "right"]
+                            angle = { "front" : 0, "left" : -90.0 , "right" : 90.0 }
+                            distance = {"front" : 10.0, "left" : 3.0, "right" : 3.0}
+                            width = {"front" : 2 ,  "left" : 2.4, "right" : 2.4}
+                            vehicle_location = vehicle.get_location()
+                            for i in order:
+                                slope = math.tan(math.radians(yaw + angle[i]))
+                                interval_point = carla.Location(ego_location.x + distance[i] * math.cos(math.radians(yaw + angle[i])) , ego_location.y + distance[i] * math.sin(math.radians(yaw + angle[i])) ,0.0)
+                                left_symmetry = carla.Location(- width[i] * slope * math.sqrt(1/ (slope **2 + 1)) + interval_point.x , width[i] * math.sqrt( 1 / (slope **2 + 1)) + interval_point.y ,1.0)
+                                right_symmetry = carla.Location( width[i] * slope * math.sqrt( 1 / (slope**2 + 1)) + interval_point.x ,- width[i] * math.sqrt(1  / (slope**2 + 1)) + interval_point.y ,1.0)
+                                front_diff = (vehicle_location.x - interval_point.x) / slope + interval_point.y - vehicle_location.y
+                                back_diff = (vehicle_location.x - ego_location.x) / slope + ego_location.y - vehicle_location.y
+                                left_diff = (vehicle_location.x - left_symmetry.x ) * slope + left_symmetry.y - vehicle_location.y
+                                right_diff = (vehicle_location.x - right_symmetry.x) * slope + right_symmetry.y - vehicle_location.y
+                                left_slope_diff = slope * ((left_symmetry.y - right_symmetry.y)/(left_symmetry.x - right_symmetry.x))
+                                print(i,left_slope_diff)
+                                world.debug.draw_string( interval_point,"M", draw_shadow=False, color=carla.Color(225,0,0), life_time=-1.0)
+                                world.debug.draw_string( left_symmetry,"L", draw_shadow=False, color=carla.Color(225,0,0), life_time=-1.0)
+                                world.debug.draw_string( right_symmetry,"R", draw_shadow=False, color=carla.Color(225,0,0), life_time=-1.0)
+                                if(front_diff * back_diff < 0 and left_diff * right_diff < 0):
+                                    control.brake = 1
+                                    ego_vehicle.apply_control(control)
+                                    left_slope_diff = slope * ((left_symmetry.y - right_symmetry.y)/(left_symmetry.x - right_symmetry.x))
+                                    break
 
-
-                            # 전방
-
-                            x_c = interval_front_point.x
-                            y_c = interval_front_point.y
-                            x_0 = ego_vehicle.get_location().x
-                            y_0 = ego_vehicle.get_location().y
-                            interval_rect = []
-                            front_diff = (vehicle.get_location().x - x_c) / math.tan(math.radians(yaw)) + y_c - vehicle.get_location().y
-                            back_diff =  (vehicle.get_location().x - x_0) / math.tan(math.radians(yaw)) + y_0 - vehicle.get_location().y
-                            front_l_p = carla.Location(- 2 *math.tan(math.radians(yaw)) * math.sqrt(1/ (math.tan(math.radians(yaw))**2 + 1)) + x_c, math.sqrt(4/ (math.tan(math.radians(yaw))**2 + 1)) + y_c ,0.0)
-                            front_r_p = carla.Location(  2 *math.tan(math.radians(yaw)) * math.sqrt( 1 / (math.tan(math.radians(yaw))**2 + 1)) + x_c,- math.sqrt(4 / (math.tan(math.radians(yaw))**2 + 1)) + y_c ,0.0)
-                            left_diff = (vehicle.get_location().x - front_l_p.x) * math.tan(math.radians(yaw)) + front_l_p.y - vehicle.get_location().y
-                            right_diff = (vehicle.get_location().x - front_r_p.x) * math.tan(math.radians(yaw)) + front_r_p.y - vehicle.get_location().y
-                            
-
-                            # 왼쪽 방
-                            left_x_c = interval_left_point.x
-                            left_y_c = interval_left_point.y
-                            left_left_diff = (vehicle.get_location().x - left_x_c) / math.tan(math.radians(yaw - 90.0)) + left_y_c - vehicle.get_location().y
-                            left_right_diff = (vehicle.get_location().x - x_0) / math.tan(math.radians(yaw - 90.0)) + y_0 - vehicle.get_location().y
-                            left_front_l_p =  carla.Location(- 2.4 *math.tan(math.radians(yaw-90.0)) * math.sqrt(1/ (math.tan(math.radians(yaw-90.0))**2 + 1)) + left_x_c, math.sqrt(6/ (math.tan(math.radians(yaw-90.0))**2 + 1)) + left_y_c ,0.0)
-                            left_front_r_p = carla.Location( 2.4 *math.tan(math.radians(yaw-90.0)) * math.sqrt(1/ (math.tan(math.radians(yaw-90.0))**2 + 1)) + left_x_c, math.sqrt(6/ (math.tan(math.radians(yaw-90.0))**2 + 1)) + left_y_c ,0.0)
-                            left_front_diff = (vehicle.get_location().x - left_front_l_p.x) * math.tan(math.radians(yaw - 90.0)) + left_front_l_p.y - vehicle.get_location().y
-                            left_back_diff =(vehicle.get_location().x - left_front_r_p.x) * math.tan(math.radians(yaw - 90.0)) + left_front_r_p.y - vehicle.get_location().y
-
-                            # 오른쪽 방
-
-                            right_x_c = interval_right_point.x
-                            right_y_c = interval_right_point.y
-                            right_right_diff = (vehicle.get_location().x - right_x_c) / math.tan(math.radians(yaw + 90.0)) + right_y_c - vehicle.get_location().y
-                            right_left_diff = (vehicle.get_location().x - x_0) / math.tan(math.radians(yaw + 90.0)) + y_0 - vehicle.get_location().y
-                            right_front_l_p =  carla.Location(- 2.4 *math.tan(math.radians(yaw + 90.0)) * math.sqrt(1/ (math.tan(math.radians(yaw+90.0))**2 + 1)) + right_x_c, math.sqrt(6/ (math.tan(math.radians(yaw+90.0))**2 + 1)) + right_y_c ,0.0)
-                            right_front_r_p = carla.Location( 2.4 *math.tan(math.radians(yaw+90.0)) * math.sqrt(1/ (math.tan(math.radians(yaw+90.0))**2 + 1)) + right_x_c, math.sqrt(6/ (math.tan(math.radians(yaw+90.0))**2 + 1)) + right_y_c ,0.0)
-                            right_front_diff = (vehicle.get_location().x - right_front_l_p.x) * math.tan(math.radians(yaw + 90.0)) + right_front_l_p.y - vehicle.get_location().y
-                            right_back_diff =(vehicle.get_location().x - right_front_r_p.x) * math.tan(math.radians(yaw + 90.0)) + right_front_r_p.y - vehicle.get_location().y
-
-
-
-                            if(left_diff * right_diff < 0 and back_diff * front_diff < 0):
-                                print("brake!!!")
-                            elif(left_left_diff * left_right_diff < 0 and left_back_diff * left_front_diff < 0):
-                                print("brake!!!!!")
-                            elif(right_left_diff * right_right_diff < 0 and right_back_diff * right_front_diff < 0):
-                                print("brake!!!!!!")
-                            # interval_rect.append(carla.Location(- 3*math.tan(math.radians(yaw)) * math.sqrt(1/ (math.tan(math.radians(yaw))**2 + 1)) + x_c, math.sqrt(9/ (math.tan(math.radians(yaw))**2 + 1)) + y_c ,0.0))
-                            # interval_rect.append(carla.Location( 3*math.tan(math.radians(yaw)) *math.sqrt(1/ (math.tan(math.radians(yaw))**2 + 1)) + x_c,- math.sqrt(9 / (math.tan(math.radians(yaw))**2 + 1)) + y_c ,0.0))
-                            # interval_rect.append(carla.Location(-3*math.tan(math.radians(yaw)) * math.sqrt(1/ (math.tan(math.radians(yaw))**2 + 1)) + x_0, math.sqrt(9 / (math.tan(math.radians(yaw))**2 + 1)) + y_0 ,0.0))
-                            # interval_rect.append(carla.Location( 3*math.tan(math.radians(yaw)) *math.sqrt(1/ (math.tan(math.radians(yaw))**2 + 1)) + x_0,- math.sqrt(9/ (math.tan(math.radians(yaw))**2 + 1)) + y_0 ,0.0))
-                            # print("m", (interval_rect[0].y - interval_rect[1].y) / (interval_rect[0].x - interval_rect[1].x)  * math.tan(math.radians(yaw)))
-
-                            # for i in range(4):
-                            #     world.debug.draw_string( interval_rect[i] + carla.Location(z=3.0),"H", draw_shadow=False, color=carla.Color(255,i*40,0), life_time=-1.0)
-                            world.debug.draw_string( interval_left_point + carla.Location(z=3.0),"H", draw_shadow=False, color=carla.Color(5,40,220), life_time=-1.0)
-                            # world.debug.draw_string( ego_vehicle.get_location() + carla.Location(z=3.0),"H", draw_shadow=False, color=carla.Color(5,220,0), life_time=-1.0)
                 for walker in world.get_actors().filter('walker.*'):
                     # draw Box 
                     if(ego_vehicle.id != walker.id and walker.is_alive):
